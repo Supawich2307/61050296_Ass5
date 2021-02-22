8bf0f7032ef433de441d479de0843452c5726561
Supawich2307 <kewlate2307@gmail.com>
2021-02-22 16:36:00 +0700

    Assingment5_JMS

---
+++ ProducerBall/src/java/producerball/Main.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package producerball;
+
+import java.util.Scanner;
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+/**
+ *
+ * @author supakew
+ */
+public class Main {
+    @Resource(mappedName = "jms/SimpleJMSTopic")
+    private static Topic topic;
+    @Resource(mappedName = "jms/ConnectionFactory")
+    private static ConnectionFactory connectionFactory;
+    @Resource(mappedName = "jms/SimpleJMSQueue")
+    private static Queue queue;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final int NUM_MSGS;
+        Connection connection = null;
+
+        if ((args.length < 1) || (args.length > 2)) {
+            System.err.println(
+                    "Program takes one or two arguments: "
+                    + "<dest_type> [<number-of-messages>]");
+            System.exit(1);
+        }
+
+        String destType = args[0];
+        System.out.println("Destination type is " + destType);
+
+        if (!(destType.equals("queue") || destType.equals("topic"))) {
+            System.err.println("Argument must be \"queue\" or " + "\"topic\"");
+            System.exit(1);
+        }
+
+        if (args.length == 2) {
+            NUM_MSGS = (new Integer(args[1])).intValue();
+        } else {
+            NUM_MSGS = 1;
+        }
+
+        Destination dest = null;
+
+        try {
+            if (destType.equals("queue")) {
+                dest = (Destination) queue;
+            } else {
+                dest = (Destination) topic;
+            }
+        } catch (Exception e) {
+            System.err.println("Error setting destination: " + e.toString());
+            System.exit(1);
+        }
+
+        
+        try {
+            connection = connectionFactory.createConnection();
+
+            Session session = connection.createSession(
+                        false,
+                        Session.AUTO_ACKNOWLEDGE);
+
+            MessageProducer producer = session.createProducer(dest);
+            TextMessage message = session.createTextMessage();
+            System.out.println("To end program, type Q or q, " + "then <return>");
+            
+            while(true){
+                System.out.print("Enter Live Score : ");
+                String msg = sc.nextLine();
+                if(msg.equals("q")||msg.equals("Q")){
+                    break;
+                }else{
+                    message.setText(msg);
+                    producer.send(message);
+                } 
+            }
+            
+            
+            producer.send(session.createMessage());
+        } catch (JMSException e) {
+            System.err.println("Exception occurred: " + e.toString());
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                }
+            }
+        }
+    }   // end main
+}//end class
